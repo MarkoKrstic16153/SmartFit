2250e3acb7b1b2ffc736ab622b444844e4c196c2
vladeta97 <vladeta.manic@elfak.rs>
2020-02-03 17:15:55 +0100

    gg2

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { ReactiveFormsModule , FormsModule} from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/PrijavaKomponente/login/login.component";
@@ -10,18 +10,16 @@ import { ProfilInstruktorComponent } from "./components/PrikazKomponente/profil-
 import { ProfilKlijentComponent } from "./components/PrikazKomponente/profil-klijent/profil-klijent.component";
 import { VezbaComponent } from "./components/PrikazKomponente/vezba/vezba.component";
 import { DodajVezbuComponent } from "./components/DodajKomponente/dodaj-vezbu/dodaj-vezbu.component";
-import { HranaComponent } from "./components/PrikazKomponente/hrana/hrana.component";
 import { DodajHranuComponent } from "./components/DodajKomponente/dodaj-hranu/dodaj-hranu.component";
 import { DodajPlanTreningaComponent } from "./components/DodajKomponente/dodaj-plan-treninga/dodaj-plan-treninga.component";
 import { DodajPlanIshraneComponent } from "./components/DodajKomponente/dodaj-plan-ishrane/dodaj-plan-ishrane.component";
 import { PlanIshraneComponent } from "./components/PrikazKomponente/plan-ishrane/plan-ishrane.component";
 import { PlanTreningaComponent } from "./components/PrikazKomponente/plan-treninga/plan-treninga.component";
 import { NavigationComponent } from "./components/PrikazKomponente/navigation/navigation.component";
-import { MaterialModule } from './material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChartsModule } from 'ng2-charts';
-import { SmartSearchBarComponent } from './components/PrikazKomponente/smart-search/smart-search-bar.component';
-
+import { MaterialModule } from "./material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ChartsModule } from "ng2-charts";
+import { SmartSearchBarComponent } from "./components/PrikazKomponente/smart-search/smart-search-bar.component";
 
 @NgModule({
   declarations: [
@@ -32,7 +30,6 @@ import { SmartSearchBarComponent } from './components/PrikazKomponente/smart-sea
     ProfilKlijentComponent,
     VezbaComponent,
     DodajVezbuComponent,
-    HranaComponent,
     DodajHranuComponent,
     DodajPlanTreningaComponent,
     DodajPlanIshraneComponent,
