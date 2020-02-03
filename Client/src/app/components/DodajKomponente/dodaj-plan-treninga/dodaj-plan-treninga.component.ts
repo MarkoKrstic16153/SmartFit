cb6d66341c41707e466d0eb9ccabc4ae6437171c
vladeta97 <vladeta.manic@elfak.rs>
2020-02-03 13:52:45 +0100

    plan ishrane prikaz delimicno

--- Client/src/app/components/DodajKomponente/dodaj-plan-treninga/dodaj-plan-treninga.component.ts
+++ Client/src/app/components/DodajKomponente/dodaj-plan-treninga/dodaj-plan-treninga.component.ts
@@ -1,158 +1,216 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Location } from '@angular/common';
-import { Router, ActivatedRoute } from '@angular/router';
-import { LoginService } from 'src/services/LoginService';
-import { FormControl, Validators } from '@angular/forms';
-import { Vezba } from 'src/models/Vezba';
-import { Observable } from 'rxjs';
-import { Trening } from 'src/models/Trening';
-import { VezbeService } from 'src/services/VezbeService';
-import { PlanTrenignaService } from 'src/services/PlanTreningaService';
-import { OdradjenaVezba } from 'src/models/OdradjenjaVezba';
-import { PlanTreninga } from 'src/models/PlanTreninga';
-import {Subject} from 'rxjs';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Location } from "@angular/common";
+import { Router, ActivatedRoute } from "@angular/router";
+import { LoginService } from "src/services/LoginService";
+import { FormControl, Validators } from "@angular/forms";
+import { Vezba } from "src/models/Vezba";
+import { Observable } from "rxjs";
+import { Trening } from "src/models/Trening";
+import { VezbeService } from "src/services/VezbeService";
+import { PlanTrenignaService } from "src/services/PlanTreningaService";
+import { OdradjenaVezba } from "src/models/OdradjenjaVezba";
+import { PlanTreninga } from "src/models/PlanTreninga";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-dodaj-plan-treninga',
-  templateUrl: './dodaj-plan-treninga.component.html',
-  styleUrls: ['./dodaj-plan-treninga.component.css']
+  selector: "app-dodaj-plan-treninga",
+  templateUrl: "./dodaj-plan-treninga.component.html",
+  styleUrls: ["./dodaj-plan-treninga.component.css"]
 })
 export class DodajPlanTreningaComponent implements OnInit {
   changingValue: Subject<Vezba[]> = new Subject();
-  usernameKlijenta:string = "";
-  usernameInstruktora:string = "";
-  smartSearch:boolean = false;
-  poslatPlanFlag:boolean = false;
-  detaljiVezba:number = -1;
-  detaljiTrening:number = -1;
-  pretragaPoImenu:string = "Pretrazite Vezbu : ";
-  obsVezbe:Observable<Vezba[]> = null;
-  trenutnaVezba:Vezba = null;
-  trenutniTrening:Trening = {odmorIzmedjuSerija:0,trajanje:0,vezbe:[],vrsta:""};
-  nizTreninga:Trening[] = [];
-  prethodniPlanovi:any[]=[];
-  nizMogucihTreninga:string[] = ['FullBody','Push','Pull','Legs','BroArms','BroChest','Calisthenics','Odmor'];
-  treningControl : FormControl = new FormControl("", Validators.required);
-  serijeControl : FormControl = new FormControl("", Validators.required);
-  opterecenjeControl : FormControl = new FormControl("", Validators.required);
-  ponavljanjaControl : FormControl = new FormControl("", Validators.required);
-  odmorIzmedjuSerijaControl : FormControl = new FormControl("", Validators.required);
-  trajanjeControl : FormControl = new FormControl("", Validators.required);
-  datumControl : FormControl = new FormControl("", Validators.required);
-  nazivControl : FormControl = new FormControl("", Validators.required);
-  bodyControl : FormControl = new FormControl("", Validators.required);
+  usernameKlijenta: string = "";
+  usernameInstruktora: string = "";
+  smartSearch: boolean = false;
+  poslatPlanFlag: boolean = false;
+  detaljiVezba: number = -1;
+  detaljiTrening: number = -1;
+  pretragaPoImenu: string = "Pretrazite Vezbu : ";
+  obsVezbe: Observable<Vezba[]> = null;
+  trenutnaVezba: Vezba = null;
+  trenutniTrening: Trening = {
+    odmorIzmedjuSerija: 0,
+    trajanje: 0,
+    vezbe: [],
+    vrsta: ""
+  };
+  nizTreninga: Trening[] = [];
+  prethodniPlanovi: any[] = [];
+  nizMogucihTreninga: string[] = [
+    "FullBody",
+    "Push",
+    "Pull",
+    "Legs",
+    "BroArms",
+    "BroChest",
+    "Calisthenics",
+    "Odmor"
+  ];
+  treningControl: FormControl = new FormControl("", Validators.required);
+  serijeControl: FormControl = new FormControl("", Validators.required);
+  opterecenjeControl: FormControl = new FormControl("", Validators.required);
+  ponavljanjaControl: FormControl = new FormControl("", Validators.required);
+  odmorIzmedjuSerijaControl: FormControl = new FormControl(
+    "",
+    Validators.required
+  );
+  trajanjeControl: FormControl = new FormControl("", Validators.required);
+  datumControl: FormControl = new FormControl("", Validators.required);
+  nazivControl: FormControl = new FormControl("", Validators.required);
+  bodyControl: FormControl = new FormControl("", Validators.required);
 
-  constructor(private route:ActivatedRoute,private router:Router,private location:Location,private loginService:LoginService,private vezbaService:VezbeService,private planTreningaService:PlanTrenignaService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private loginService: LoginService,
+    private vezbaService: VezbeService,
+    private planTreningaService: PlanTrenignaService
+  ) {}
 
   ngOnInit() {
-    let podaci = this.route.snapshot.paramMap.get('data');
-    var res : string[] = podaci.split(" ");
+    let podaci = this.route.snapshot.paramMap.get("data");
+    var res: string[] = podaci.split(" ");
     console.log(res[0] + res[1]);
     this.usernameInstruktora = res[1];
-    this.usernameKlijenta = res [0];
-    this.planTreningaService.getAllPlanTreninga(this.usernameKlijenta,this.usernameInstruktora).subscribe((data)=>{
-      this.prethodniPlanovi = data;
-    });
+    this.usernameKlijenta = res[0];
+    this.planTreningaService
+      .getAllPlanTreninga(this.usernameKlijenta, this.usernameInstruktora)
+      .subscribe(data => {
+        this.prethodniPlanovi = data;
+      });
   }
 
-  daLiJeLogovan():boolean{
-    if(this.loginService.logovaniUsername!="")
-      return true;
-    else
-      return false;
+  daLiJeLogovan(): boolean {
+    if (this.loginService.logovaniUsername != "") return true;
+    else return false;
   }
 
-  dodajVezbu(){
+  dodajVezbu() {
     console.log(this.bodyControl.value);
-    if((this.opterecenjeControl.value!="" || (this.bodyControl.value == true && this.opterecenjeControl.value!=null)) && this.serijeControl.value!="" && this.ponavljanjaControl.value!="" && this.trenutnaVezba!=null){
+    if (
+      (this.opterecenjeControl.value != "" ||
+        (this.bodyControl.value == true &&
+          this.opterecenjeControl.value != null)) &&
+      this.serijeControl.value != "" &&
+      this.ponavljanjaControl.value != "" &&
+      this.trenutnaVezba != null
+    ) {
       let opterecenjePom;
-      if(this.opterecenjeControl.value=="" || this.opterecenjeControl.value==null)
+      if (
+        this.opterecenjeControl.value == "" ||
+        this.opterecenjeControl.value == null
+      )
         opterecenjePom = 0;
-      else
-        opterecenjePom = this.opterecenjeControl.value;
-      let novaOdradjenaVezba : OdradjenaVezba = {brojPonavljanja:this.ponavljanjaControl.value,brojSerija:this.serijeControl.value,opterecenje:opterecenjePom,vezba:this.trenutnaVezba};
+      else opterecenjePom = this.opterecenjeControl.value;
+      let novaOdradjenaVezba: OdradjenaVezba = {
+        brojPonavljanja: this.ponavljanjaControl.value,
+        brojSerija: this.serijeControl.value,
+        opterecenje: opterecenjePom,
+        vezba: this.trenutnaVezba
+      };
       this.trenutniTrening.vezbe.push(novaOdradjenaVezba);
       console.log(this.trenutniTrening);
     }
   }
 
-  selektujVezbu(vezba:Vezba){
+  selektujVezbu(vezba: Vezba) {
     console.log(vezba);
     this.trenutnaVezba = vezba;
   }
 
-  selektujTipTreninga(){
+  selektujTipTreninga() {
     console.log(this.treningControl.value);
     this.smartSearch = true;
     this.fetchujTipVezbi();
   }
 
   fetchujTipVezbi() {
-    if(this.treningControl.value!="Odmor"){
-      this.obsVezbe=this.vezbaService.getTipTreningaVezbe(this.treningControl.value);
-      this.obsVezbe.subscribe((data)=>{
+    if (this.treningControl.value != "Odmor") {
+      this.obsVezbe = this.vezbaService.getTipTreningaVezbe(
+        this.treningControl.value
+      );
+      this.obsVezbe.subscribe(data => {
         this.changingValue.next(data);
       });
     }
   }
 
-  back(){
+  back() {
     this.location.back();
   }
 
-  zavrsiTrening(){
-    if(this.treningControl.value == "" || this.treningControl.value == "Odmor" || this.trenutniTrening.vezbe.length==0){
+  zavrsiTrening() {
+    if (
+      this.treningControl.value == "" ||
+      this.treningControl.value == "Odmor" ||
+      this.trenutniTrening.vezbe.length == 0
+    ) {
       this.trenutniTrening.trajanje = 0;
       this.trenutniTrening.odmorIzmedjuSerija = 0;
       this.trenutniTrening.vrsta = "Odmor";
       this.nizTreninga.push(this.trenutniTrening);
-      this.trenutniTrening = {odmorIzmedjuSerija:0,trajanje:0,vezbe:[],vrsta:""};
-    }
-    else if(this.odmorIzmedjuSerijaControl.value!="" && this.trajanjeControl.value!=""){
-    this.trenutniTrening.trajanje = this.trajanjeControl.value;
-    this.trenutniTrening.odmorIzmedjuSerija = this.odmorIzmedjuSerijaControl.value;
-    this.trenutniTrening.vrsta = this.treningControl.value;
-    this.nizTreninga.push(this.trenutniTrening);
-    this.trenutniTrening = {odmorIzmedjuSerija:0,trajanje:0,vezbe:[],vrsta:""};
+      this.trenutniTrening = {
+        odmorIzmedjuSerija: 0,
+        trajanje: 0,
+        vezbe: [],
+        vrsta: ""
+      };
+    } else if (
+      this.odmorIzmedjuSerijaControl.value != "" &&
+      this.trajanjeControl.value != ""
+    ) {
+      this.trenutniTrening.trajanje = this.trajanjeControl.value;
+      this.trenutniTrening.odmorIzmedjuSerija = this.odmorIzmedjuSerijaControl.value;
+      this.trenutniTrening.vrsta = this.treningControl.value;
+      this.nizTreninga.push(this.trenutniTrening);
+      this.trenutniTrening = {
+        odmorIzmedjuSerija: 0,
+        trajanje: 0,
+        vezbe: [],
+        vrsta: ""
+      };
     }
   }
 
-  posaljiPlan(){
-    if(this.datumControl.value!="" && this.nazivControl.value!=""){
+  posaljiPlan() {
+    if (this.datumControl.value != "" && this.nazivControl.value != "") {
       console.log("salje plan");
-      let noviPlanTreninga : PlanTreninga = {
-        datum:this.datumControl.value,
-        naziv:this.nazivControl.value,
-        usernameInstruktora:this.usernameInstruktora,
-        usernameKlijenta:this.usernameKlijenta,
-        treninzi:this.nizTreninga,
+      let noviPlanTreninga: PlanTreninga = {
+        datum: this.datumControl.value,
+        naziv: this.nazivControl.value,
+        usernameInstruktora: this.usernameInstruktora,
+        usernameKlijenta: this.usernameKlijenta,
+        treninzi: this.nizTreninga,
         komentari: []
       };
       console.log(noviPlanTreninga);
-      this.planTreningaService.addPlanTreninga(noviPlanTreninga).subscribe((data)=>{
-        this.planTreningaService.getAllPlanTreninga(this.usernameKlijenta,this.usernameInstruktora).subscribe((data)=>{
-          this.prethodniPlanovi = data;
+      this.planTreningaService
+        .addPlanTreninga(noviPlanTreninga)
+        .subscribe(data => {
+          this.planTreningaService
+            .getAllPlanTreninga(this.usernameKlijenta, this.usernameInstruktora)
+            .subscribe(data => {
+              this.prethodniPlanovi = data;
+            });
         });
-      });
       this.poslatPlanFlag = true;
       this.nizTreninga = [];
     }
   }
 
   daLiJeGotov(): boolean {
-    if (this.nizTreninga.length >= 7)
-     return true;
-    else 
-      return false;
+    if (this.nizTreninga.length >= 7) return true;
+    else return false;
   }
 
-  prikaziDetaljeVezba(index:number){
+  prikaziDetaljeVezba(index: number) {
     console.log(index);
     if (index == this.detaljiVezba) this.detaljiVezba = -1;
     else this.detaljiVezba = index;
   }
 
-  obrisiVezba(vezba:OdradjenaVezba){
+  obrisiVezba(vezba: OdradjenaVezba) {
     console.log(vezba);
     const index = this.trenutniTrening.vezbe.indexOf(vezba);
     if (index > -1) {
@@ -160,13 +218,13 @@ export class DodajPlanTreningaComponent implements OnInit {
     }
   }
 
-  prikaziDetaljeTrening(index:number){
+  prikaziDetaljeTrening(index: number) {
     console.log(index);
     if (index == this.detaljiTrening) this.detaljiTrening = -1;
     else this.detaljiTrening = index;
   }
 
-  obrisiTrening(trening:Trening){
+  obrisiTrening(trening: Trening) {
     console.log(trening);
     const index = this.nizTreninga.indexOf(trening);
     if (index > -1) {
@@ -174,16 +232,26 @@ export class DodajPlanTreningaComponent implements OnInit {
     }
   }
 
-  vratiTreningUString(index:number):string{
-    if(this.nizTreninga[index].vezbe.length>0)
-      return "Dan "+ (index + 1) + " -> Tip Treninga : " + this.nizTreninga[index].vrsta + ", Broj Vezbi : " + this.nizTreninga[index].vezbe.length;
-    else 
-      return "Dan " + (index + 1) + " -> Dan Odmora.";
+  vratiTreningUString(index: number): string {
+    if (this.nizTreninga[index].vezbe.length > 0)
+      return (
+        "Dan " +
+        (index + 1) +
+        " -> Tip Treninga : " +
+        this.nizTreninga[index].vrsta +
+        ", Broj Vezbi : " +
+        this.nizTreninga[index].vezbe.length
+      );
+    else return "Dan " + (index + 1) + " -> Dan Odmora.";
   }
 
   odvediNaPlan(plan: any) {
-    let param = {uK:this.usernameKlijenta,uI:this.usernameInstruktora,dat:plan.datum,naz:plan.naziv};
-    this.router.navigate(["prikazplantreninga",JSON.stringify(param)]);
+    let param = {
+      uK: this.usernameKlijenta,
+      uI: this.usernameInstruktora,
+      dat: plan.datum,
+      naz: plan.naziv
+    };
+    this.router.navigate(["prikazplantreninga", JSON.stringify(param)]);
   }
-
 }
--- Client/src/app/components/PrikazKomponente/plan-ishrane/plan-ishrane.component.ts
+++ Client/src/app/components/PrikazKomponente/plan-ishrane/plan-ishrane.component.ts
@@ -1,39 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from 'src/services/LoginService';
-import { PlanIshraneService } from 'src/services/PlanIshraneService';
-import { HranaService } from 'src/services/HranaService';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { PlanIshrane } from 'src/models/PlanIshrane';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "src/services/LoginService";
+import { PlanIshraneService } from "src/services/PlanIshraneService";
+import { HranaService } from "src/services/HranaService";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+import { PlanIshrane } from "src/models/PlanIshrane";
+import { identifierModuleUrl } from "@angular/compiler";
+import { stringify } from "querystring";
 
 @Component({
-  selector: 'app-plan-ishrane',
-  templateUrl: './plan-ishrane.component.html',
-  styleUrls: ['./plan-ishrane.component.css']
+  selector: "app-plan-ishrane",
+  templateUrl: "./plan-ishrane.component.html",
+  styleUrls: ["./plan-ishrane.component.css"]
 })
 export class PlanIshraneComponent implements OnInit {
-  parametri:any={};
-  plan:PlanIshrane=null;
-  constructor(private loginService : LoginService,private planIshraneService:PlanIshraneService,private hranaService:HranaService,private router:Router,private route: ActivatedRoute,private location :Location) { }
-  
+  parametri: any = {};
+  plan: PlanIshrane = null;
+  detaljiObrok: boolean = false;
+  constructor(
+    private loginService: LoginService,
+    private planIshraneService: PlanIshraneService,
+    private hranaService: HranaService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
   ngOnInit() {
-    let data=this.route.snapshot.paramMap.get('data');
+    let data = this.route.snapshot.paramMap.get("data");
     this.parametri = JSON.parse(data);
-    this.planIshraneService.getPlanIshrane(this.parametri.uK,this.parametri.uI,this.parametri.dat,this.parametri.naz).subscribe((data)=>{
-      this.plan = data;
-      console.log(this.plan);
-    });
+    this.planIshraneService
+      .getPlanIshrane(
+        this.parametri.uK,
+        this.parametri.uI,
+        this.parametri.dat,
+        this.parametri.naz
+      )
+      .subscribe(data => {
+        this.plan = data;
+        console.log(this.plan);
+      });
   }
 
-  daLiJeLogovan():boolean{
-    if(this.loginService.logovaniUsername!="")
-      return true;
-    else
-      return false;
+  daLiJeLogovan(): boolean {
+    if (this.loginService.logovaniUsername != "") return true;
+    else return false;
   }
 
-  goBack(){
+  goBack() {
     this.location.back();
   }
 
+  kastujDan(dan: any): string {
+    let vrednost: string;
+    if (dan == 1) vrednost = "Ponedeljak";
+    else if (dan == 2) vrednost = "Utorak";
+    else if (dan == 3) vrednost = "Sreda";
+    else if (dan == 4) vrednost = "Cetvrtak";
+    else if (dan == 5) vrednost = "Petak";
+    else if (dan == 6) vrednost = "Subota";
+    else vrednost = "Nedelja";
+
+    return vrednost;
+  }
+
+  prikaziDetaljeObroka() {
+    console.log(this.detaljiObrok);
+    if (this.detaljiObrok == false) this.detaljiObrok = true;
+    else this.detaljiObrok = false;
+  }
+
+  prikaziKalorijeObroka(obrok: any): number {
+    let vrati: number = 0;
+    obrok.deloviObroka.forEach(deoObroka => {
+      vrati +=
+        deoObroka.hrana.proteini * 4 +
+        deoObroka.hrana.ugljeniHidrati * 4 +
+        deoObroka.hrana.masti * 9;
+    });
+    vrati.toFixed(1);
+    return vrati;
+  }
 }
