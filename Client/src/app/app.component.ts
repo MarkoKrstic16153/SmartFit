5122fb14352023fccbe88e9ee301d44b50b0bcff
Sayrax97 <dusann.jankovic@elfak.rs>
2020-02-05 16:24:28 +0100

    Home page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { LoginService } from "src/services/LoginService";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Client';
+  title = "SmartFit";
+  constructor(private router: Router, private loginService: LoginService) {}
+  getNavBarClasses(): string {
+    if (this.router.url == "/") {
+      return "navbar navbar-expand-lg navbar-dark opacity position-fixed";
+    } else {
+      return "navbar navbar-expand-lg navbar-light";
+    }
+  }
+  daLiJeLogovan(): boolean {
+    if (this.loginService.logovaniUsername != "") return true;
+    else return false;
+  }
+  logout() {
+    this.loginService.logovaniUsername = "";
+    this.loginService.korisnik = true;
+    this.router.navigate(["/"]);
+  }
 }
